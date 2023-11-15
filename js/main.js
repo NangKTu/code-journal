3e0ca3457dadd3e37b844bc9ebc13f182d363c5b
Nang K Tu <ntu2391007@gmail.com>
2023-11-15 22:23:42 +0000

    Completed issue-2-user-can-view-their-entries -Nang Tu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
+/* global data */
+
+const $entriesView = document.getElementById('entries');
+const $entryFormView = document.getElementById('entry-form');
+
 document.querySelector('#photo-url').addEventListener('input', function (e) {
   const imageUrl = this.value;
   document.querySelector('#uploaded-img').src = imageUrl;
 });
 
 document.querySelector('#entryForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+
   const $title = document.querySelector('#title').value;
   const $photoUrl = document.querySelector('#photo-url').value;
   const $notes = document.querySelector('#notes').value;
@@ -18,6 +25,12 @@ document.querySelector('#entryForm').addEventListener('submit', function (e) {
   data.nextEntryId++;
   // add new entry to beginning of the data
   data.entries.unshift(newEntry);
+  // Render a DOM tree for the newly submitted entry
+  renderEntry(newEntry);
+  // Show the "entries" view
+  viewSwap('entries');
+  // Conditionally use the toggleNoEntries function to remove the no entries text
+  toggleNoEntries(data.entries.length === 0);
   // to reset preview image to placeholder image
   document.querySelector('#uploaded-img').src =
     '/images/placeholder-image-square.jpg';
@@ -47,12 +60,66 @@ function renderEntry(entry) {
   $div.appendChild($p);
 
   $li.appendChild($div);
+  const $entriesList = document.getElementById('entriesList');
+  $entriesList.appendChild($li);
 }
 
 document.addEventListener('DOMContentLoaded', function (e) {
   const $entriesList = document.getElementById('entriesList');
-  for (let i = 0; i < data.entries.length; i++) {
-    const entryEle = renderEntry(data.entries[i]);
-    $entriesList.appendChild(entryEle);
+  $entriesList.innerHTML = '';
+
+  // Show the view which was displayed prior to page refresh
+  viewSwap(data.view);
+
+  if (data.entries.length > 0) {
+    for (let i = 0; i < data.entries.length; i++) {
+      renderEntry(data.entries[i]);
+    }
+  } else {
+    const noEntries = document.createElement('p');
+    noEntries.textContent = 'No entries have been recorded.';
+    $entriesList.appendChild(noEntries);
   }
+
+  // Conditionally use the toggleEntries function to show or remove the no entries text
+  toggleNoEntries(data.entries.length === 0);
+});
+
+function toggleNoEntries(visible) {
+  const $entriesList = document.getElementById('entriesList');
+  const $noEntries = document.getElementById('noEntries');
+  if ($noEntries) {
+    $noEntries.style.display = visible ? 'block' : 'none';
+    $entriesList.style.display = visible ? 'none' : 'block';
+  }
+}
+
+function viewSwap(viewName) {
+  if (viewName === 'entries') {
+    $entriesView.classList.remove('hidden');
+    $entryFormView.classList.add('hidden');
+  } else if (viewName === 'entry-form') {
+    $entryFormView.classList.remove('hidden');
+    $entriesView.classList.add('hidden');
+  }
+  data.view = viewName;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  viewSwap(data.view);
+
+  // Add an event listener to the Entries
+  document
+    .getElementById('entries-link')
+    .addEventListener('click', function (e) {
+      e.preventDefault();
+      viewSwap('entries');
+    });
+  // Add an event listener to New
+  document
+    .getElementById('new-entry-link')
+    .addEventListener('click', function (e) {
+      e.preventDefault();
+      viewSwap('entry-form');
+    });
 });
