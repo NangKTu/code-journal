ecaa2e94dd807941ca904d9b8502300caf4f3d65
Nang K Tu <ntu2391007@gmail.com>
2023-11-16 03:24:42 +0000

    edit page is done

--- js/main.js
+++ js/main.js
@@ -40,6 +40,7 @@ document.querySelector('#entryForm').addEventListener('submit', function (e) {
 
 function renderEntry(entry) {
   const $li = document.createElement('li');
+  $li.setAttribute('data-entry-id', entry.entryId);
 
   const $img = document.createElement('img');
   $img.setAttribute('class', 'savedImage');
@@ -52,6 +53,10 @@ function renderEntry(entry) {
   const $h2 = document.createElement('h2');
   $h2.setAttribute('class', 'h2Test');
   $h2.textContent = entry.$title;
+  // Add a pencil icon next to the title
+  const $pencilIcon = document.createElement('i');
+  $pencilIcon.setAttribute('class', 'fa-solid fa-pencil');
+  $h2.appendChild($pencilIcon);
   $div.appendChild($h2);
 
   const $p = document.createElement('p');
@@ -61,7 +66,7 @@ function renderEntry(entry) {
 
   $li.appendChild($div);
   const $entriesList = document.getElementById('entriesList');
-  $entriesList.appendChild($li);
+  $entriesList.prepend($li);
 }
 
 document.addEventListener('DOMContentLoaded', function (e) {
@@ -108,18 +113,57 @@ function viewSwap(viewName) {
 document.addEventListener('DOMContentLoaded', function () {
   viewSwap(data.view);
 
-  // Add an event listener to the Entries
+  // Event listener for Entries link
   document
     .getElementById('entries-link')
     .addEventListener('click', function (e) {
       e.preventDefault();
       viewSwap('entries');
     });
-  // Add an event listener to New
+
+  // Event listener for New entry link
   document
     .getElementById('new-entry-link')
     .addEventListener('click', function (e) {
       e.preventDefault();
       viewSwap('entry-form');
     });
+});
+
+document.addEventListener('click', function (event) {
+  const clickedPencil = event.target.closest('.fa-pencil');
+
+  if (clickedPencil) {
+    // Find the closest parent li element (the entry) that has a data-entry-id attribute
+    const $entry = clickedPencil.closest('li[data-entry-id]');
+
+    if ($entry) {
+      // Extract the data-entry-id attribute value
+      const entryId = $entry.getAttribute('data-entry-id');
+
+      // Find the corresponding entry in the data.entries array
+      const editingEntry = data.entries.find(
+        (entry) => entry.entryId === parseInt(entryId)
+      );
+
+      if (editingEntry) {
+        // Assign the editing entry to the data.editing property
+        data.editing = editingEntry;
+
+        // Show the form view
+        viewSwap('entry-form');
+
+        // Pre-populate the form fields with the editing entry's values
+        document.querySelector('#title').value = editingEntry.$title;
+        document.querySelector('#photo-url').value = editingEntry.$photoUrl;
+        document.querySelector('#notes').value = editingEntry.$notes;
+
+        // Set the preview image to the editing entry's photo URL
+        document.querySelector('#uploaded-img').src = editingEntry.$photoUrl;
+
+        // Update the title of the entry-form view to Edit Entry
+        document.querySelector('#editEntries').innerText = 'Edit Entry';
+      }
+    }
+  }
 });
